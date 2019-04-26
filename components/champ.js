9682e18f0181ed1540d15411eb828d5f4eb912bf
unknown <vipvodanhmaster@gmail.com>
2019-04-26 23:26:14 +0700

    fix flatlist

--- components/champ.js
+++ components/champ.js
@@ -1,11 +1,20 @@
 import  React,{Component} from 'react'
 import {View,Text,Image,StyleSheet} from 'react-native'
 export default class Champ extends Component{
+  constructor(props){
+    super(props);
+  }
   render(){
+
+    const img='http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/';
+    const img2=this.props.infoChamp.image.full;
+    const img3=img+img2;
     return(
-      <View>
+       <View>
         <Image
+        resizeMode='cover'
         style={styles.image}
+        source={{uri:img3}}
         />
         <Text>{this.props.infoChamp.name}</Text>
       </View>
